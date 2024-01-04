5cc119bdbc31b9b22b8e77e2229862180a402cc4
aburahan8721 <aburaihan8721@gmail.com>
2024-01-04 10:49:21 +0600

    ui done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+
+// import Register from "./pages/Register";
+
+// import ProductList from "./pages/ProductList";
+
+// import Product from "./pages/Product";
+
+// import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Cart /> */}
+      <Home />
+      {/* <Login /> */}
+      {/* <Product /> */}
+      {/* <ProductList /> */}
+      {/* <Register /> */}
+    </div>
+  );
+};
+
+export default App;
